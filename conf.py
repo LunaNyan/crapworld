5a8463963067494352cb1d63f011ac77743f98fe
libertin <cinnamoroll@erpin.club>
2024-09-30 10:23:54 +0900

    docs

--- conf.py
+++ conf.py
@@ -1,3 +1,6 @@
+# 웹 서버 설정
+# 사이트 설정은 data에 있다.
+
 # Flask dev mode가 켜지고 디버그 탭이 활성화된다.
 # !! : 해당 변수와 관계없이 터미널에 표출되는 로그의 레벨은 DEBUG로 설정된다.
 debug = False
@@ -6,6 +9,8 @@ debug = False
 # 개발 환경이 아닌 경우 False로 놓는 것을 권장한다.
 dynamically_reload_site_settings = True
 
-# 개발 환경에서만 사용된다.
+# 서버 호스트
 listen_host = "0.0.0.0"
+# 서버 포트
+# Docker 환경에서는 11111로 둔다.
 listen_port = 11111
