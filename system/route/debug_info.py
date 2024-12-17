9e323e693646bd49ef1cc235e39aeda7ef7adf91
libertin <cinnamoroll@erpin.club>
2024-12-17 15:05:00 +0900

    add exception handler for debug_info will not crash app if .git not found

--- system/route/debug_info.py
+++ system/route/debug_info.py
@@ -1,4 +1,5 @@
 from system.engine.server import app
+from system.engine.log_manager import logger as log
 from system.engine.mgmt import BOOT_AT
 from system.engine.settings import site_settings
 from system.tool import renderer
@@ -10,7 +11,17 @@ import conf
 import git
 import locale
 
-repo = git.Repo(search_parent_directories=True)
+try:
+    repo = git.Repo(search_parent_directories=True)
+    git_sha = repo.head.object.hexsha[-10:]
+except git.exc.InvalidGitRepositoryError:
+    if conf.debug:
+        log.warn("Git Repository is invalid. Git SHA is unavailable on Debug Info")
+    git_sha = "알 수 없음 (InvalidGitRepositoryError)"
+except git.exc.NoSuchPathError:
+    if conf.debug:
+        log.warn("Git Path is invalid. Git SHA is unavailable on Debug Info")
+    git_sha = "알 수 없음 (NoSuchPathError)"
 
 
 @app.route('/debug')
@@ -24,7 +35,7 @@ def debug_info():
     <b>Uptime</b> : {datetime.now() - BOOT_AT}<br>
     <b>Version</b> : {VERSION}<br>
     <b>Branch</b> : {repo.active_branch}<br>
-    <b>Git SHA</b> : {repo.head.object.hexsha[-10:]}<br><br>
+    <b>Git SHA</b> : {git_sha}<br><br>
     <b>OS</b> : {platform.system()} {platform.release()} ({platform.machine()})<br>
     <b>Locale</b> : {locale.getlocale(locale.LC_CTYPE)}<br>
     <b>Python</b> : {platform.python_implementation()} {platform.python_version()} ({platform.python_revision()})<br>
